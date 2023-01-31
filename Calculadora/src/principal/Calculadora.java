0bbb56de3b86f49b68bdaeba8198694ca7431c32
ikaslea <ikaslea@hz110aab.zornotza.local>
2023-01-31 12:41:57 +0100

    Ejercicio Javadoc - Calculadora

--- Calculadora/src/principal/Calculadora.java
+++ Calculadora/src/principal/Calculadora.java
@@ -1,13 +1,38 @@
 package principal;
 
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import menu.Menu;
 import operaciones.Operaciones;
 
 public class Calculadora {
+	private static final Logger LOGGER = Logger.getLogger(Calculadora.class.getName());
+
 	public static void main(String[] args) {
 		/*
 		 * 
 		 */
+		Handler consoleHandler = null;
+		Handler fileHandler = null;
+		try {
+
+			consoleHandler = new ConsoleHandler();
+			fileHandler = new FileHandler("./ficheroLog.log");
+			LOGGER.setUseParentHandlers(false);
+			LOGGER.addHandler(consoleHandler);
+			LOGGER.addHandler(fileHandler);
+
+			consoleHandler.setLevel(Level.WARNING);
+			fileHandler.setLevel(Level.ALL);
+			LOGGER.setLevel(Level.ALL);
+		} catch (IOException exception) {
+			LOGGER.log(Level.SEVERE, "Ocurrió un error en FileHandler.", exception);
+		}
 		int resultado = 0;
 		String operacion = "";
 		int[] operandos = new int[2];
@@ -15,33 +40,34 @@ public class Calculadora {
 		Menu menu = new Menu();
 		Operaciones operaciones = new Operaciones();
 
-		try {
-			do {
-				operandos = menu.pedirNumeros();
-				operacion = menu.menuOpciones();
+		do {
+			operandos = menu.pedirNumeros();
+			operacion = menu.menuOpciones();
 
-				if (operacion.equalsIgnoreCase("+")) {
-					resultado = operaciones.sumar(operandos[0], operandos[1]);
-					System.out.println("Resultado: " + resultado);
-				} else if (operacion.equalsIgnoreCase("-")) {
-					resultado = operaciones.restar(operandos[0], operandos[1]);
-					System.out.println("Resultado: " + resultado);
-				} else if (operacion.equalsIgnoreCase("*")) {
-					resultado = operaciones.multiplicar(operandos[0], operandos[1]);
-					System.out.println("Resultado: " + resultado);
-				} else if (operacion.equalsIgnoreCase("/")) {
+			if (operacion.equalsIgnoreCase("+")) {
+				resultado = operaciones.sumar(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+			} else if (operacion.equalsIgnoreCase("-")) {
+				resultado = operaciones.restar(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+			} else if (operacion.equalsIgnoreCase("*")) {
+				resultado = operaciones.multiplicar(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+			} else if (operacion.equalsIgnoreCase("/")) {
+				try {
 					resultado = operaciones.dividir(operandos[0], operandos[1]);
 					System.out.println("Resultado: " + resultado);
-				} else if (operacion.equalsIgnoreCase("%")) {
-					resultado = operaciones.resto(operandos[0], operandos[1]);
-					System.out.println("Resultado: " + resultado);
-				} else {
-					System.out.println("Operaci�n no v�lida");
+				} catch (ArithmeticException e) {
+					LOGGER.log(Level.WARNING, "divición entre cero", resultado);
+					System.out.println("intentalo de nuevo");
 				}
-			} while (menu.repetir());
-		} catch (ArithmeticException e) {
-			System.out.println("error aritmético " + e.getMessage());
-			System.out.println("fin del programa");
-		}
+			} else if (operacion.equalsIgnoreCase("%")) {
+				resultado = operaciones.resto(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+			} else {
+				System.out.println("Operaci�n no v�lida");
+			}
+		} while (menu.repetir());
+
 	}
 }
